2de3cafdfef8219b0e2d4ac1956a780351c0e734
Evgeny Lychkovsky <miphreal@gmail.com>
2015-03-21 19:50:40 +0300

    updated i3 config; added rofi

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-rm -Rf $HOME/.i3 $HOME/.config/i3 $HOME/.config/compton.conf $HOME/.config/dunst
+rm -Rf $HOME/.i3 $HOME/.config/i3 $HOME/.config/compton.conf $HOME/.config/dunst $HOME/.config/i3status
 
 ln -fs `pwd` $HOME/.config/i3 || true
 ln -fs $HOME/.config/i3/compton.conf $HOME/.config/compton.conf || true
 ln -fs $HOME/.config/i3/dunst $HOME/.config/dunst
+ln -fs $HOME/.config/i3/i3status $HOME/.config/i3status
--- install.sh
+++ install.sh
@@ -6,3 +6,16 @@ apt-get --allow-unauthenticated install i3-autobuild-keyring
 apt-get update
 
 apt-get install compton feh i3 dunst
+
+# rofi
+# $ sudo apt-get install libpango1.0-dev libxinerama-dev libxft-dev libx11-dev dh-make
+# $ wget https://github.com/DaveDavenport/rofi/releases/download/0.15.2/rofi-0.15.2.tar.gz
+# $ tar xf rofi-0.15.2.tar.gz
+# $ cd rofi-0.15.2
+# $ dh_make --single --native --copyright mit
+###The step make test was failing for me, so I disabled it by adding a trivial rule in debian/rules:
+## override_dh_auto_test:
+##     echo "Skipping test..."
+##And then continue to build the .deb as usual:
+# $ dpkg-buildpackage -uc -us -rfakeroot
+# $ dpkg -i  ../rofi_0.15.2_amd64.deb    # Install and enjoy...
